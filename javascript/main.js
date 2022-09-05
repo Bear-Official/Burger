a7fbbc49ff105dd46c84fb0f364f9b2416507d2f
Bear <danekgdgdg@gmail.com>
2022-09-05 16:02:01 +0300

    [Fixed] a bug with menu

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+let body = document.getElementsByClassName("body")[0];
+let button = document.getElementsByClassName("menu")[0];
+console.log(button.checked);
+button.onclick = (event) => {
+    if (button.checked == true) {
+        body.style = "overflow: hidden;";
+    } else if (button.checked == false) {
+        body.style = "overflow: scroll;";
+    }
+};
