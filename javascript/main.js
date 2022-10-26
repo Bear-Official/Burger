cfb54aeeb198a1ce170239b8b4ff726e7dde4ffe
Bear <danekgdgdg@gmail.com>
2022-10-26 14:36:41 +0300

    [Fixed] some errors

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,6 @@ AOS.init();
 
 let body = document.getElementsByClassName("body")[0];
 let button = document.getElementsByClassName("menu")[0];
-console.log(button.checked);
 button.onclick = (event) => {
     if (button.checked == true) {
         body.style = "overflow: hidden;";
